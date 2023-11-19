b7f891e243fb7efa0f13f32c02bd1685485b7c42
Ian da Silva Correia <54759457+IanDesc@users.noreply.github.com>
2023-11-19 04:03:21 -0300

    Added Header

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 "use client";
+import CustomHeader from "@/components/CustomHeaderHeader";
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <main className="flex-auto bg-white">
       <Navbar selectedTab="Inicio" />
+      <CustomHeader title="Inicio" />
       <div className=" w-screen bg-white" />
     </main>
   );
