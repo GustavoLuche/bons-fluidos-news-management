0c9a9fc905aebc7138294bd911c351d6d9ff9040
Gustavo Luche <92736531+GustavoLuche@users.noreply.github.com>
2023-11-21 00:00:13 -0300

    Added Banner

--- src/app/about.js
+++ src/app/about.js
@@ -1,6 +1,7 @@
 "use client";
 import CustomHeader from "@/components/CustomHeaderHeader";
 import Navbar from "@/components/Navbar";
+import Banner from "@/components/Banner";
 import Image from "next/image";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
     <main className="flex-auto bg-white">
       <Navbar selectedTab="Sobre nós" />
       <CustomHeader title="About" />
+      <Banner />
       <div className=" w-screen bg-white" />
     </main>
   );
