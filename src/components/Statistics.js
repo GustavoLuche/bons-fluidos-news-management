dce747992da085004bc44e7cccd4d17c77781ecf
Gustavo Luche <92736531+GustavoLuche@users.noreply.github.com>
2023-11-22 02:58:54 -0300

    Update Statistcs Component

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,7 +1,10 @@
 const stats = [
-    { id: 1, name: 'Transactions every 24 hours', value: '44 million' },
-    { id: 2, name: 'Assets under holding', value: '$119 trillion' },
-    { id: 3, name: 'New users annually', value: '46,000' },
+    { id: 1, name: 'De pessoas mestruam em todo o planeta', value: '1.8 BILHÃO' },
+    { id: 2, name: 'De pessoas não tem acesso a itens de cuidados mestuiais', value: '+4 MILHÕES' },
+    { id: 3, name: 'Pessoas que mestruam vivem sem acesso a banheiro em casa', value: '713 MIL' },
+    { id: 4, name: 'De pessoas no Brasil não tem banheiro em casa', value: '1.6 MILHÃO' },
+    { id: 5, name: 'Não recebem água tratada em casa', value: '15 MILHÕES' },
+    { id: 6, name: 'De estudantes no Brasil deixam de ir à escola por não terem absorvente', value: '25%' },
   ]
   
   export default function Statistics() {
