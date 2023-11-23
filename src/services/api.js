36cf830c52d599a8c21d3f4f95b5f3605b2b0857
Matheus Peres <146034272+matheuspmb@users.noreply.github.com>
2023-11-22 22:06:43 -0300

    mudança de style newsletter & conexão com banco de dados

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,31 @@
-// import axios from "";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
 
-// export const api = axios.create({
-//   baseURL: "",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
+const firebaseConfig = {
+  apiKey: "AIzaSyA-UF3gSE1lZFJvFgqVhBxHEsmJXrqH02s",
+  authDomain: "precise-airship-398418.firebaseapp.com",
+  projectId: "precise-airship-398418",
+  storageBucket: "precise-airship-398418.appspot.com",
+  messagingSenderId: "271597864169",
+  appId: "1:271597864169:web:f2f3fdef59cd4eb56b5c78",
+  measurementId: "G-XHGXL4MQ00"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const subscribeToNewsletter = async (email) => {
+  try {
+    const newsletterRef = collection(db, "newsletter");
+    await addDoc(newsletterRef, {
+      email,
+    });
+    console.log("E-mail cadastrado com sucesso no Firebase!");
+    return true;
+  } catch (error) {
+    console.error("Erro ao cadastrar o e-mail no Firebase:", error);
+    return false;
+  }
+};
+
+export { subscribeToNewsletter, db };
