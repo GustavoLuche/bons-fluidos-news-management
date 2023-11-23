d4c4eb0d3d46a866135cd6db05aae51f4ff246a4
Ian da Silva Correia <54759457+IanDesc@users.noreply.github.com>
2023-11-23 01:38:25 -0300

    feat/Finished Login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -12,7 +12,7 @@ export default function Login() {
   const [email, setEmail] = useState("");
   const [error, setError] = useState(false);
   const [user, setUser] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [password, setPassword] = useState("");
   const router = useRouter();
 
@@ -38,19 +38,17 @@ export default function Login() {
   // }, [user]);
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-6">
-      <div className="flex flex-col items-center justify-center min-h-screen bg-Magenta hidden sm:block order-last sm:order-first sm:col-span-4 overflow-hidden h-screen ">
+    <div className="grid grid-cols-1 lg:grid-cols-6">
+      <div className="flex flex-col items-center justify-center min-h-screen bg-Magenta hidden lg:block order-last lg:order-first lg:col-span-3 overflow-hidden h-screen ">
         <Image
           src={background}
           alt="Imagem de fundo contendo logo do Bons fluidos e uma mulher com os braços abertos em liberto"
-          // width={2000}
-          // height={1050}
-          className="w-screen h-screen absolute"
+          className="w-screen h-screen absolute object-cover"
           priority
         />
       </div>
 
-      <div className="col-span-2 ">
+      <div className="col-span-3 ">
         <div className="relative w-full px-6 flex flex-col h-screen items-center justify-self-center justify-center bg-gradient-to-b from-RoseLighter to-White">
           <Image
             src={logo}
@@ -63,12 +61,33 @@ export default function Login() {
           {!user && (
             <div>
               <h1 className="text-xl text-center font-semibold mb-4 text-Black mt-10">
-                Faz login ai
+                Olá!
               </h1>
+              <div className="flex flex-col items-center justify-center">
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="mb-3 min-w-[200px] w-auto flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-Black shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-Magenta sm:text-sm sm:leading-6"
+                  placeholder="Seu email"
+                />
+
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="password"
+                  required
+                  className="mb-3 min-w-[200px] w-auto flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-Black shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-Magenta sm:text-sm sm:leading-6"
+                  placeholder="Sua senha"
+                />
+              </div>
 
               <div className="mt-1 ">
                 <button
-                  className="flex mx-auto items-center justify-center drop-shadow-[0_0_0.3rem_#ffffff70] bg-gradient-to-b text-Black from-transparent backdrop-blur-2xl bg-Magenta static w-auto rounded-xl px-6 py-4 w-100"
+                  className="w-full flex mx-auto items-center justify-center drop-shadow-[0_0_0.3rem_#ffffff70] bg-gradient-to-b text-White from-transparent backdrop-blur-2xl bg-Magenta static w-auto rounded-xl px-6 py-4 w-200"
                   onClick={signIn}
                 >
                   {loading ? (
@@ -86,7 +105,7 @@ export default function Login() {
                   —— OU ——
                 </h1>
                 <button
-                  className="col-span-3 mt-4 mx-auto flex items-center justify-center drop-shadow-[0_0_0.3rem_#ffffff70] bg-gradient-to-b text-Black from-transparent backdrop-blur-2xl bg-white static w-auto rounded-xl px-6 py-4 "
+                  className="col-span-3 mt-4 mx-auto flex items-center justify-center drop-shadow-[0_0_0.3rem_#ffffff70] bg-gradient-to-b text-Black from-transparent backdrop-blur-2xl bg-white static w-auto rounded-xl px-6 py-4 hover:underline "
                   onClick={() => handleOpenRegister()}
                 >
                   Cadastre-se
