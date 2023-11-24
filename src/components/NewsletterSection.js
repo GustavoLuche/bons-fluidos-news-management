7173a80fdb897e6f6e251212b8ba839a755e51ba
Gustavo Luche <92736531+GustavoLuche@users.noreply.github.com>
2023-11-24 19:00:22 -0300

    Fix input text color in email field

--- src/components/NewsletterSection.js
+++ src/components/NewsletterSection.js
@@ -46,7 +46,7 @@ export default function NewsletterSection() {
                 type="email"
                 autoComplete="email"
                 required
-                className="min-w-0 flex-auto rounded-md border-0 bg-white text-black px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-Rose sm:text-sm sm:leading-6"
+                className="min-w-0 flex-auto rounded-md border-0 bg-white text-black px-3.5 py-2 shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-Rose sm:text-sm sm:leading-6"
                 placeholder="Digite seu email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
