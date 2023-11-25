9bdcec6d5664ee0d3d074e8ea2cededf8bbbf7c9
Kainã de Freitas Richalski <kaina-richalski@hotmail.com>
2023-11-25 02:50:03 -0300

    atualiação news feed

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,30 +1,35 @@
 import React, { useEffect, useState } from "react";
 import { getPosts } from "@/services/api";
 
-const Post = ({ post }) => (
-  <div className="md:flex mb-2 border border-gray-300 bg-white rounded-xl shadow-md overflow-hidden">
-    <div className="md:shrink-0">
-      <a href={`/news/${post.id}`}>
+const Post = ({ post }) => {
+  const handleClick = () => {
+    window.location.href = `/news/${post.id}`;
+  };
+
+  return (
+    <div
+      className="md:flex mb-2 border border-gray-300 bg-white rounded-xl shadow-md overflow-hidden cursor-pointer"
+      onClick={handleClick}
+    >
+      <div className="md:shrink-0">
         <img
           src={post.previewImageUrl}
           alt={post.title}
           className="h-48 w-full object-cover md:h-full md:w-48"
         />
-      </a>
-    </div>
-    <div className="p-8">
-      <div className="uppercase tracking-wide text-sm font-semibold text-Magenta">
-        {post.tag}
       </div>
-      <h3 className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
-        <a href={post.href} className="text-black hover:underline">
+      <div className="p-8">
+        <div className="uppercase tracking-wide text-sm font-semibold text-Magenta">
+          {post.tag}
+        </div>
+        <h3 className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
           {post.title}
-        </a>
-      </h3>
-      <p className="mt-2 text-black">{post.description}</p>
+        </h3>
+        <p className="mt-2 text-black">{post.description}</p>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 const NewsFeed = () => {
   const [posts, setPosts] = useState([]);
